34a65095eb348df08eac3021371bcf676192ac1d
C.J. O'Leary <cjoleary17@gmail.com>
2023-08-10 16:48:52 -0600

    sec 10 mod 88

---
+++ assets/product-model.js
@@ -0,0 +1,42 @@
+class ProductModel extends HTMLElement {
+    constructor() {
+        super();
+        this.openModelModal();
+    }
+
+    getMediaID() {
+        const id = this.getAttribute('data-media-id');
+
+        return id;
+    }
+
+    getModal() {
+        const modal = document.getElementById('productModelModal');
+
+        return modal;
+    }
+
+    openModelModal() {
+        const mediaID = this.getMediaID();
+        const modal = this.getModal();
+
+        if(!mediaID) return;
+
+        const openModalBtn = document.getElementById(`productModelOpenBtn_${mediaID}`);
+
+        openModalBtn.addEventListener('click', function(e) {
+            const modalBody = modal.querySelector('#body');
+
+            modalBody.innerHTML = ''
+
+            const template = document.querySelector(`product-model[data-media-id="${mediaID}"] > template`);
+
+            const clone = template.content.cloneNode(true);
+
+            modalBody.appendChild(clone);
+            modalBody.querySelector('model-viewer').setAttribute('reveal', 'auto');
+        });
+    }
+}
+
+const productModal = customElements.define('product-model', ProductModel);
