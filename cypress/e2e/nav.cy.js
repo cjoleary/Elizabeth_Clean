cefc75076016b6c090af666eda60c07492ca1503
C.J. O'Leary <cjoleary17@gmail.com>
2023-08-16 15:32:44 -0600

    nav responsiveness test

---
+++ cypress/e2e/nav.cy.js
@@ -0,0 +1,35 @@
+/// <reference types="cypress" />
+
+describe('main site navigation', () => {
+    beforeEach(() => {
+      cy.visit('/');
+      cy.get('#password').type('Coleworld2324!');
+      cy.get('button[type="submit"').click({force: true});
+      cy.visit('/');
+    });
+    
+    it('checks nav responsiveness', () => {
+        // mobile
+        cy.viewport('iphone-xr');
+        cy.get('[data-cy="desktop-nav"]').should('not.be.visible');
+        cy.get('[data-cy="open-mobile-nav"]').should('be.visible');
+
+        // desktop
+        cy.viewport('macbook-15');
+        cy.get('[data-cy="desktop-nav"]').should('be.visible');
+        cy.get('[data-cy="open-mobile-nav"]').should('not.be.visible');
+    });
+
+    it('opens and closes the mobile nav', () => {
+        cy.viewport('iphone-xr');
+        // open
+        cy.get('[data-cy="open-mobile-nav"]').click();
+        cy.screenshot();
+        cy.get('[data-cy="mobile-nav"]').should('be.visible');
+
+        // close
+        cy.get('[data-cy="close-mobile-nav"]').click();
+        cy.screenshot();
+        cy.get('[data-cy="mobile-nav"]').should('not.be.visible');
+    });
+  });
